fa5d5a9d5f012c040e3f49345c5a2f05ac9505a7
Zahid Efendic <zahid.efendic96@gmail.com>
2025-11-13 11:48:16 +0100

    Prva PHP vjezba

---
+++ uvod.php
@@ -0,0 +1,19 @@
+<?php
+
+echo "Helo world";
+
+$ime = "Zahid Efendic"; //Ovo je string (uvijek idu pod navodnicima)
+$godine = 30; // Ovo je integer (broj ide bez navodnika)
+$visina = 183.2; // Ovo je float (decimalni broj )
+$programer = true; // ovo je boolean, $programer = 1
+
+echo "Moje ime je $ime, i imam $godine godina, a visok sam $visina.";
+
+$visina = 185.5;
+$ime = "Zahid";
+$godine = 22; 
+
+echo "Moje ime $ime, i imam $godine,  a visok sam  $visina.";
+
+
+?>
